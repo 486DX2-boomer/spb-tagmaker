ad57b7b8ff8f64ece4f8d8a95783824337f32dd8
Daniel Feller <dfeller@thesoundofit.com>
2020-10-29 15:00:56 -0400

    Added support for used gun tags

--- main.go
+++ main.go
@@ -19,19 +19,16 @@ func main() {
 	// The web server runs indefinitely, waiting for new tags or for the user to delete all tags in memory and start over.
 
 	// Creating a pdf
-	pdf := gofpdf.New("P", "pt", "A4", "")
-	pdf.AddUTF8Font(SPBTagFont, "", "framd.ttf")
-	pdf.SetFont(SPBTagFont, "", 12) // Call set font with a default size to avoid compiler error
 
 	// pdf.AddPage()
 
 	var l List
 
-	l = append(l, NewTag("Walther", "PPK", "380 Auto", true, "779", Small))
-	l = append(l, NewTag("Colt", "M4 Carbine", "5.56", true, "1099", Big))
+	l = append(l, NewTag("Walther", "PPK", "380 Auto", false, "779", Small))
+	l = append(l, NewTag("Colt", "M4 Carbine", "5.56mm", false, "1099", Big))
 	l = append(l, NewTag("Springfield", "Saint", "5.56mm", true, "879", Big))
 
-	l = append(l, NewTag("Glock", "G44", "22 LR", true, "389", Small))
+	l = append(l, NewTag("Glock", "G44", "22 LR", false, "389", Small))
 	l = append(l, NewTag("Smith & Wesson", "617", "22 LR", true, "709", Small))
 	l = append(l, NewTag("Kel Tec", "PMR 30", "22 WMR", true, "399", Small))
 
@@ -42,18 +39,15 @@ func main() {
 	l = append(l, NewTag("FN", "FNX-45 Tactical", "45 ACP", true, "1229", Small))
 	l = append(l, NewTag("CZ", "97B", "45 ACP", true, "719", Small))
 	l = append(l, NewTag("Smith & Wesson", "M&P 40 FDE", "40 S&W", true, "699", Small))
-	l = append(l, NewTag("GSG", "GSG-16 Carbine", "22 LR", true, "349", Big))
+	l = append(l, NewTag("GSG", "GSG-16 Carbine", "22 LR", false, "349", Big))
 	l = append(l, NewTag("HK", "HK 45c", "45 ACP", true, "779", Small))
 
-	BuildDocument(l, pdf)
+	BuildDocument(l, NewDocument())
 
-	err := pdf.OutputFileAndClose("output.pdf")
-	fmt.Println(err)
-	fmt.Println("PDF generated")
 }
 
 // BuildDocument accepts a list of tags and builds a full document, sorting the handgun and long gun tags and then drawing each tag to an appropriate page.
-// BuildDocument must be supplied with a pointer to a gofpdf object which should be created on program initialization
+// BuildDocument must be supplied with a pointer to a gofpdf object which can be supplied with the NewDocument() function
 func BuildDocument(inputList []Tag, pdf *gofpdf.Fpdf) {
 
 	longGunPageCoord := GetLongGunPageCoord()
@@ -84,4 +78,18 @@ func BuildDocument(inputList []Tag, pdf *gofpdf.Fpdf) {
 		handgunTags[i].Draw(handGunPageCoord[(i%10)], pdf)
 	}
 
+	err := pdf.OutputFileAndClose("output.pdf")
+	fmt.Println(err)
+	fmt.Println("PDF generated")
+
+}
+
+// NewDocument initializes a pdf, sets the font and font size, and then returns the pdf. This is meant to be used in conjunction with BuildDocument()
+func NewDocument() *gofpdf.Fpdf {
+	pdf := gofpdf.New("P", "pt", "A4", "")
+	pdf.AddUTF8Font(SPBTagFont, "", "framd.ttf")
+	pdf.SetFont(SPBTagFont, "", 12) // Call set font with a default size to avoid compiler error
+	pdf.SetTextColor(0, 0, 0)
+
+	return pdf
 }
--- tags.go
+++ tags.go
@@ -33,16 +33,8 @@ func NewTag(manufacturer string, model string, caliber string, new bool, price s
 
 // Draw the tag to the position coordinate provided
 func (t Tag) Draw(zero Coord, pdf *gofpdf.Fpdf) {
-	// draw a long gun tag
-	if t.TagSize == Big {
-		// // draw 3x5 guides
-		// pdf.MoveTo(125, 72)
-		// pdf.LineTo((125 + 360), 72)
-		// pdf.ClosePath()
-		// pdf.SetLineWidth(0.5)
-		// pdf.DrawPath("D")
-
-		// pdf.SetXY(125, 72) // 0, 0 coordinate for tag 1
+	if t.TagSize == Big && t.New == true {
+		// draw a long gun tag
 
 		// needed to set up image embed
 		var opt gofpdf.ImageOptions
@@ -107,7 +99,7 @@ func (t Tag) Draw(zero Coord, pdf *gofpdf.Fpdf) {
 		pdf.SetDrawColor(SeparatorBarColorR, SeparatorBarColorB, SeparatorBarColorG)
 		pdf.DrawPath("D")
 
-	} else if t.TagSize == Small {
+	} else if t.TagSize == Small && t.New == true {
 		// draw a handgun tag
 
 		// needed to set up image embed
@@ -171,6 +163,183 @@ func (t Tag) Draw(zero Coord, pdf *gofpdf.Fpdf) {
 		pdf.SetLineWidth(SeparatorBarLineWidth)
 		pdf.SetDrawColor(SeparatorBarColorR, SeparatorBarColorB, SeparatorBarColorG)
 		pdf.DrawPath("D")
+	} else if t.TagSize == Big && t.New == false {
+		// draw a used long gun tag
+
+		// needed to set up image embed
+		var opt gofpdf.ImageOptions
+
+		// Draw Preowned Firearm in top left
+		pdf.SetFillColor(254, 0, 0)
+		pdf.SetDrawColor(254, 0, 0)
+		pdf.MoveTo(zero.X, zero.Y)
+		pdf.LineTo((zero.X + 128), zero.Y)
+		pdf.CurveTo(zero.X+150, zero.Y+80, zero.X, zero.Y+70)
+		pdf.ClosePath()
+		pdf.DrawPath("F")
+		pdf.SetTextColor(255, 255, 255)
+		pdf.SetXY(zero.X+8, zero.Y+20)
+		pdf.SetFont(SPBTagFont, "", 20)
+		pdf.Write(0, "Preowned")
+		pdf.SetXY(zero.X+8, zero.Y+44)
+		pdf.Write(0, "Firearm")
+		pdf.SetTextColor(0, 0, 0)
+
+		// get manufacturer logo path
+		LogoPath := (GetLogoImagePath(t.Manufacturer))
+		pdf.ImageOptions(LogoPath, (zero.X + LongGunManufacturerLogoOffsetX), (zero.Y + LongGunManufacturerLogoOffsetY), LongGunManufacturerLogoWidth, 0, false, opt, 0, "")
+
+		pdf.SetXY((zero.X + LongGunDollarSignOffsetX), (zero.Y + LongGunDollarSignOffsetY)) // Location of $ sign
+		pdf.SetFont(SPBTagFont, "", LongGunDollarFontSize)
+		pdf.Cell(45, 55, "$")
+
+		pdf.SetXY((zero.X + LongGunPriceOffsetX), (zero.Y + LongGunPriceOffsetY))
+		// fontSizeOffset is used to scale down the LongGunPriceFontSize based on number of digits in the price
+		var fontSizeOffset float64
+		if len(t.Price) <= 3 {
+			fontSizeOffset = 0
+		} else if len(t.Price) == 4 {
+			fontSizeOffset = 28
+		} else if len(t.Price) == 5 {
+			fontSizeOffset = 44
+		} else if len(t.Price) == 6 {
+			fontSizeOffset = 58
+		} else if len(t.Price) >= 7 {
+			fontSizeOffset = 72
+		}
+		pdf.SetFont(SPBTagFont, "", (LongGunPriceFontSize - fontSizeOffset))
+		// Original cell width for the price was 270 points, this needs to be adjusted down to 200 so that the 99 cents fits in the tag
+		// Need to move the cell dimensions to constants
+		// pdf.Cell(270, 109, t.Price)
+		pdf.Cell(200, 109, t.Price)
+		pdf.SetFont(SPBTagFont, "", LongGunCentsFontSize)
+		pdf.Write(LongGunCentsFontSize, "99")
+
+		pdf.SetXY((zero.X + LongGunModelOffsetX), (zero.Y + LongGunModelOffsetY))
+		// Scale down the LongGunModelFontSize based on number of characters in the model name
+		// This hand tuned offset will gracefully handle around ~44 characters before it starts overflowing, but may collide with the caliber cell if the caliber is lengthy
+		if len(t.Model) <= 12 {
+			fontSizeOffset = 0
+		} else if len(t.Model) > 12 && len(t.Model) < 18 {
+			fontSizeOffset = 4
+		} else if len(t.Model) > 18 {
+			fontSizeOffset = 8
+		}
+		pdf.SetFont(SPBTagFont, "", (LongGunModelFontSize - fontSizeOffset))
+		pdf.CellFormat(113, 18, t.Model, "", 0, "L", false, 0, "")
+
+		// On used guns, the caliber is written in white on a red semi circle
+		pdf.SetFillColor(254, 0, 0)
+		pdf.SetDrawColor(254, 0, 0)
+		pdf.MoveTo(zero.X+360, zero.Y+216)
+		pdf.LineTo(zero.X+360, zero.Y+164)
+		pdf.CurveTo(zero.X+238, zero.Y+140, zero.X+220, zero.Y+216)
+		pdf.ClosePath()
+		pdf.DrawPath("F")
+
+		pdf.SetTextColor(255, 255, 255)
+		pdf.SetXY((zero.X + LongGunCaliberOffsetX), (zero.Y + LongGunCaliberOffsetY))
+		// Scale down the LongGunCaliberFontSize based on number of characters in the caliber designation
+		if len(t.Caliber) <= 6 {
+			fontSizeOffset = 0
+		} else if len(t.Caliber) > 6 && len(t.Caliber) < 12 {
+			fontSizeOffset = 4
+		} else if len(t.Caliber) > 12 {
+			fontSizeOffset = 8
+		}
+		pdf.SetFont(SPBTagFont, "", LongGunCaliberFontSize-fontSizeOffset)
+		pdf.CellFormat(113, 18, t.Caliber, "", 0, "R", false, 0, "")
+		pdf.SetTextColor(0, 0, 0)
+
+	} else if t.TagSize == Small && t.New == false {
+		// draw a used handgun tag
+
+		// needed to set up image embed
+		var opt gofpdf.ImageOptions
+
+		// Draw Preowned Firearm in top left
+		pdf.SetFillColor(254, 0, 0)
+		pdf.SetDrawColor(254, 0, 0)
+		pdf.MoveTo(zero.X, zero.Y)
+		pdf.LineTo((zero.X + 90), zero.Y)
+		pdf.CurveTo(zero.X+100, zero.Y+64, (zero.X), zero.Y+50)
+		pdf.ClosePath()
+		pdf.DrawPath("F")
+		pdf.SetTextColor(255, 255, 255)
+		pdf.SetXY(zero.X+4, zero.Y+14)
+		pdf.SetFont(SPBTagFont, "", 14)
+		pdf.Write(0, "Preowned")
+		pdf.SetXY(zero.X+4, zero.Y+32)
+		pdf.Write(0, "Firearm")
+		pdf.SetTextColor(0, 0, 0)
+
+		// get logo path
+		LogoPath := (GetLogoImagePath(t.Manufacturer))
+
+		pdf.ImageOptions(LogoPath, (zero.X + HandgunManufacturerLogoOffsetX), (zero.Y + HandgunManufacturerLogoOffsetY), HandgunManufacturerLogoWidth, HandgunManufacturerLogoHeight, false, opt, 0, "")
+
+		pdf.SetXY((zero.X + HandgunDollarSignOffsetX), (zero.Y + HandgunDollarSignOffsetY)) // Location of $ sign
+		pdf.SetFont(SPBTagFont, "", HandgunDollarFontSize)
+		pdf.Cell(18, 28, "$")
+
+		pdf.SetXY((zero.X + HandgunPriceOffsetX), (zero.Y + HandgunPriceOffsetY))
+		// fontSizeOffset is used to scale down the HandgunPriceFontSize based on number of digits in the price
+		var fontSizeOffset float64
+		if len(t.Price) <= 3 {
+			fontSizeOffset = 0
+		} else if len(t.Price) == 4 {
+			fontSizeOffset = 12
+		} else if len(t.Price) == 5 {
+			fontSizeOffset = 24
+		} else if len(t.Price) == 6 {
+			fontSizeOffset = 30
+		} else if len(t.Price) >= 7 {
+			fontSizeOffset = 28
+		}
+		pdf.SetFont(SPBTagFont, "", (HandgunPriceFontSize - fontSizeOffset))
+		// NOTE: Remember to move the cell sizes to constants
+		pdf.CellFormat(110, 45, t.Price, "", 0, "R", false, 0, "")
+		pdf.SetFont(SPBTagFont, "", HandgunCentsFontSize)
+		// Subtracting a few points from the font size superscripts the 99 cent slightly
+		pdf.Write((HandgunCentsFontSize - 16), "99")
+
+		pdf.SetXY((zero.X + HandgunModelOffsetX), (zero.Y + HandgunModelOffsetY))
+		// Scale down the HandgunModelFontSize based on number of characters in the model name
+		// This hand tuned offset will gracefully handle around ~44 characters before it starts overflowing, but may collide with the caliber cell if the caliber is lengthy
+		if len(t.Model) <= 12 {
+			fontSizeOffset = 0
+		} else if len(t.Model) > 12 && len(t.Model) < 18 {
+			fontSizeOffset = 2
+		} else if len(t.Model) > 18 {
+			fontSizeOffset = 5
+		}
+		pdf.SetFont(SPBTagFont, "", (HandgunModelFontSize - fontSizeOffset))
+		pdf.CellFormat(60, 8, t.Model, "", 0, "L", false, 0, "")
+
+		// On used guns, the caliber is written in white on a red semi circle
+
+		pdf.SetFillColor(254, 0, 0)
+		pdf.SetDrawColor(254, 0, 0)
+		pdf.MoveTo(zero.X+216, zero.Y+144)
+		pdf.LineTo(zero.X+216, zero.Y+110)
+		pdf.CurveTo(zero.X+140, zero.Y+90, zero.X+112, zero.Y+144)
+		pdf.ClosePath()
+		pdf.DrawPath("F")
+
+		pdf.SetTextColor(255, 255, 255)
+		pdf.SetXY((zero.X + HandgunCaliberOffsetX), (zero.Y + HandgunCaliberOffsetY))
+		// Scale down the HandgunCaliberFontSize based on number of characters in the caliber designation
+		if len(t.Caliber) <= 6 {
+			fontSizeOffset = 0
+		} else if len(t.Caliber) > 9 && len(t.Caliber) < 14 {
+			fontSizeOffset = 2
+		} else if len(t.Caliber) > 14 {
+			fontSizeOffset = 4
+		}
+		pdf.SetFont(SPBTagFont, "", HandgunCaliberFontSize-fontSizeOffset)
+		pdf.CellFormat(40, 8, t.Caliber, "", 0, "R", false, 0, "")
+		pdf.SetTextColor(0, 0, 0)
+
 	}
 
 }
